e1c0df8d9fe713184f5dd913e7ea8c929fa39a2c
sergi-jr <burashnikov.sergei1@mail.ru>
2025-04-19 16:52:22 +0300

    add exception and its global handler

---
+++ src/main/java/kokodi/game/cardzen/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package kokodi.game.cardzen.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
