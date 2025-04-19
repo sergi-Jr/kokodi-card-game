32237a9ebf2a2dd2e14dd385836bba9d41f939db
sergi-jr <burashnikov.sergei1@mail.ru>
2025-04-19 16:53:41 +0300

    add jwt util class

---
+++ src/main/java/kokodi/game/cardzen/utils/JWTUtils.java
@@ -0,0 +1,28 @@
+package kokodi.game.cardzen.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Component
+@RequiredArgsConstructor
+public class JWTUtils {
+
+    private final JwtEncoder encoder;
+
+    public String generateToken(String username) {
+        Instant now = Instant.now();
+        JwtClaimsSet set = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .subject(username)
+                .build();
+        return this.encoder.encode(JwtEncoderParameters.from(set)).getTokenValue();
+    }
+}
