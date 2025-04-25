108c303c41f57f7a8e0021738a6fc0d4207bf9f6
sergi-jr <burashnikov.sergei1@mail.ru>
2025-04-25 13:28:43 +0300

    add storage info dto mapping

--- src/main/java/kokodi/game/cardzen/gamesession/dal/GameSessionMapper.java
+++ src/main/java/kokodi/game/cardzen/gamesession/dal/GameSessionMapper.java
@@ -1,10 +1,13 @@
 package kokodi.game.cardzen.gamesession.dal;
 
 import kokodi.game.cardzen.gamesession.dto.GameSessionDto;
+import kokodi.game.cardzen.gamesession.dto.GameSessionStorageInfoDto;
 import kokodi.game.cardzen.gamesession.model.GameSession;
+import kokodi.game.cardzen.gamesession.model.GameSessionStorage;
 import kokodi.game.cardzen.mapper.JsonNullableMapper;
 import kokodi.game.cardzen.mapper.ReferenceMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
@@ -14,4 +17,7 @@ public abstract class GameSessionMapper {
     public abstract GameSession toEntity(GameSessionDto gameSessionDto);
 
     public abstract GameSessionDto toGameSessionDto(GameSession gameSession);
+
+    @Mapping(target = "playersWithPoints", source = "userIdsWithPoints")
+    public abstract GameSessionStorageInfoDto toStorageInfoDto(GameSessionStorage model);
 }
